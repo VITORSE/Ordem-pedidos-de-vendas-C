282f92b0eedb87c2b9a6c06e3608e4d4283c269e
VITOR <86529895+VITORSE@users.noreply.github.com>
2023-02-20 18:39:15 -0300

    Add files via upload

---
+++ Models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrdemDeVenda.Models
+{
+    public class Cliente
+    {
+        public string Telefone { get; set; }
+        public string Endereco { get; set; }
+        public string Email { get; set; }
+    }
+}
